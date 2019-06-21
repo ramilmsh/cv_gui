56ecc8c24c3c82b2ce977267c8e521524be2497b
Ramil Shaymardanov <ramilmsh@r.local>
2019-06-21 09:36:40 +0400

    fixing streaming through pubsub

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from src.gui.VideoStreamer import GUIServer
+from src.gui.VideoStreamer import VideoStreamer
 from src.camera.Camera import Camera
 from threading import Thread
 import time
@@ -8,8 +8,8 @@ from src.utils.PubSub import PubSub
 
 Camera(0)
 
-server = GUIServer()
-server.run()
+server = VideoStreamer()
+server.run(host="0.0.0.0", threaded=True)
 
 #thread = Thread(target=server.run, kwargs={"host": "0.0.0.0", "threaded": True})
 # thread.start()
--- src/gui/VideoStreamer.py
+++ src/gui/VideoStreamer.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template, Response
 from redis import Redis
+from threading import Lock
 
 import cv2
 import numpy as np
 import os
 import time
+
+from src.utils.injection.decorator import inject
+from src.utils.PubSub import PubSub
 
 from src.camera.BaseCamera import BaseCamera
 from src.camera.Frame import Frame
@@ -15,11 +19,15 @@ class VideoStreamer(Flask):
     IMAGE_HEIGHT = 360
 
 
-    def __init__(self, camera: BaseCamera, pubsub):
+    @inject
+    def __init__(self, pubsub: PubSub = None):
         super().__init__("GUI", template_folder=VideoStreamer.DIRECTORY+"/templates",
                          static_folder=VideoStreamer.DIRECTORY+"/static")
 
         self.pubsub = pubsub
+
+        self.frame_data = None
+        self.frame_lock = Lock()
 
         self.add_url_rule('/', view_func=self.index)
         self.add_url_rule('/stream', view_func=self.stream)
@@ -31,15 +39,22 @@ class VideoStreamer(Flask):
         return Response(self._generate_message(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
     def _generate_message(self):
-        self.pubsub.subscribe("DefaultCamera")
+        self.frame_lock.acquire()
+        self.pubsub.subscribe("DefaultCamera", self._receive_data)
 
-        for message in redis_pubsub.listen():
-            if message['type'] != 'message':
-                continue
+        while True:
+            self.frame_lock.acquire()
             try:
-                frame = Frame.from_bytes(message['data'])
+                frame = Frame.from_bytes(self.frame_data)
                 frame.resize(height=VideoStreamer.IMAGE_HEIGHT)
                 yield (b'--frame\r\n'
                     b'Content-Type: image/jpeg\r\n\r\n' + cv2.imencode('.jpeg', frame.to_cv2_bgr())[1].tobytes() + b'\r\n')
             except Exception as e:
                 pass
+    
+    def _receive_data(self, data):
+        self.frame_data = data
+        try:
+            self.frame_lock.release()
+        except RuntimeError:
+            pass
