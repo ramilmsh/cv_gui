b8bc59bf4a22ef084885b4d202283da8bc757cd9
Ramil Shaymardanov <ramilmsh@r.local>
2019-06-21 08:32:29 +0400

    reverting some changes

--- src/gui/VideoStreamer.py
+++ src/gui/VideoStreamer.py
@@ -1,77 +1,45 @@
+from flask import Flask, render_template, Response
+from redis import Redis
+
 import cv2
 import numpy as np
 import os
 import time
-from threading import Event, Lock
 
 from src.camera.BaseCamera import BaseCamera
 from src.camera.Frame import Frame
-from src.utils.injection.decorator import inject
-from src.utils.PubSub import PubSub
-from src.utils.OrderedHashSet import OrderedHashSet
-
-from tornado import websocket, httpserver, ioloop, web
 
 
-class VideoStreamer(websocket.WebSocketHandler):
+class VideoStreamer(Flask):
+    DIRECTORY = os.path.dirname(os.path.realpath(__file__))
     IMAGE_HEIGHT = 360
 
-    @inject
-    def initialize(self, pubsub: PubSub = None):
+
+    def __init__(self, camera: BaseCamera, pubsub):
+        super().__init__("GUI", template_folder=VideoStreamer.DIRECTORY+"/templates",
+                         static_folder=VideoStreamer.DIRECTORY+"/static")
+
         self.pubsub = pubsub
-        self.current_frame = None
-        self.clients = {}
-        self.clients_lock = Lock()
-        self.image_lock = Lock()
 
-    def open(self):
-        self._id = self.pubsub.subscribe("DefaultCamera", self._process_frames)
-        self.clients[self._id] = self
+        self.add_url_rule('/', view_func=self.index)
+        self.add_url_rule('/stream', view_func=self.stream)
 
-    def on_message(self, message):
-        if self.current_frame is None:
-            return cv2.imread('src/camera/skins_feature.jpg')
+    def index(self):
+        return render_template('index.html', context={"title": "Title"})
 
-        self.write_message(self.current_frame)
+    def stream(self):
+        return Response(self._generate_message(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-    def on_close(self):
-        self.pubsub.unsubscribe("DefaultCamera", self._id)
-        with self.clients_lock:
-            del self.clients[self._id]
+    def _generate_message(self):
+        self.pubsub.subscribe("DefaultCamera")
 
-    def _process_frames(self, data):
-        frame = Frame.from_bytes(data)
-        frame.resize(height=VideoStreamer.IMAGE_HEIGHT)
-        data = b'--frame\r\nContent-Type: image/jpeg\r\n\r\n'\
-            + cv2.imencode('.jpeg', frame.to_cv2_bgr())[1].tobytes()\
-            + b'\r\n'
-        self.current_frame = data
-        self.image_lock.release()
-
-
-class MainHandler(web.RequestHandler):
-    def get(self):
-        items = ["Item 1", "Item 2", "Item 3"]
-        self.render("templates/index.html", title="My title")
-
-
-class GUIServer:
-
-    @inject
-    def __init__(self, pubsub: PubSub = None):
-        self.pubsub = pubsub
-        # self.cam_manager = CameraManager()
-
-    def run(self) -> object:
-        # self.cam_manager.run()
-
-        routes = [('/stream', VideoStreamer), ('/', MainHandler)]
-        application = web.Application(routes)
-
-        http_server = httpserver.HTTPServer(application)
-        http_server.listen(8888)
-        try:
-            ioloop.IOLoop.instance().start()
-        except KeyboardInterrupt:
-            http_server.stop()
-            ioloop.IOLoop.instance().stop()
+        for message in redis_pubsub.listen():
+            if message['type'] != 'message':
+                continue
+            try:
+                frame = Frame.from_bytes(message['data'])
+                frame.resize(height=VideoStreamer.IMAGE_HEIGHT)
+                yield (b'--frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n' + cv2.imencode('.jpeg', frame.to_cv2_bgr())[1].tobytes() + b'\r\n')
+            except Exception as e:
+                pass
